60b9bd99cb26193bd39a47a1352ad0c8090eb9f6
chw081 <chw081@ucsd.edu>
2023-08-21 00:57:42 +0800

    lec04 movement func

--- lec04.py
+++ lec04.py
@@ -6,29 +6,61 @@ class lec04(ThreeDScene):
         self.set_camera_orientation(phi=45*DEGREES, theta=-35*DEGREES,zoom=0.6)
         self.move_camera(frame_center= [4, -0.5, 1])
         baseCube = Cube(side_length=1, fill_opacity=1, fill_color=RED, stroke_width=0.2, stroke_color=BLACK)
+        transformShift = 4
         self.play(FadeIn(baseCube))
-        self.play(baseCube.copy().animate(run_time=1.5).shift(RIGHT * 4))
+        self.play(baseCube.copy().animate(run_time=1.5).shift(RIGHT * transformShift))
         newCubes = self.createCubes(2, BLUE).shift(UP * 3)
         self.play(FadeIn(newCubes))
         #groups = VGroup(*[newCubes[i].copy() for i in range(0, 2**3, 2)])
         groups = newCubes[0].copy()
-        groups2slt1 = VGroup(*[newCubes[1][0].copy(), newCubes[1][1].copy()])
-        groups2slt2 = VGroup(*[newCubes[1][2].copy(), newCubes[1][3].copy()])
-        self.play(AnimationGroup(*[
-            group.animate(run_time=1.5).shift(RIGHT * 5, DOWN * 2) for group in groups]),
+        groups2slt1 = VGroup(*[newCubes[1][0:2].copy()])
+        groups2slt2 = VGroup(*[newCubes[1][2:4].copy()])
+        self.play(
+            #AnimationGroup(*[
+            #group.animate(run_time=1.5).shift(RIGHT * 5, DOWN * 2) for group in groups]),
+            #groups.animate(run_time=1.5).move_to([5.5, 1.5, 0]),
+            *self.evenSquare(2, newCubes, transformShift, 1),
             AnimationGroup(*[
             group.animate(run_time=1.5).shift([4, -2, -1]) for group in groups2slt1]),
-            AnimationGroup(*[
-            group.animate(run_time=1.5).shift([4.5, -3.5, -1]) for group in groups2slt2.rotate(
-            90 * DEGREES, axis=[0, 0, 1])]))
+            #AnimationGroup(*[
+            #group.animate(run_time=1.5).shift([4.5, -3.5, -1]) for group in groups2slt2.rotate(
+            #90 * DEGREES, axis=[0, 0, 1])]))
+            groups2slt2.animate(run_time=1.5).move_to([5.5, 0, 0]).rotate(90 * DEGREES, axis=[0, 0, 1]))
         
         newCubes = self.createCubes(3, ORANGE).shift(UP * 8)
         self.play(FadeIn(newCubes))
+        self.play(*self.oddSquare(3, newCubes, transformShift, 3))
         #newCubes = self.createCubes(4, GREEN)
         #self.play(FadeIn(newCubes))
     
-    #def createLayer(self, n, layerN, newCubes):
-    #    return VGroup(*[newCubes[i].copy() for i in range(layerN, n**3, n)])
+    def evenSquare(self, n, newCubes, transformShift, prevSum):
+        groups = []
+        rightShift = transformShift + n - 0.5
+        upShift = prevSum + n//2 - 0.5
+        cubesCopy = newCubes.copy()
+        for i in range(n//2):
+            group = cubesCopy[i]
+            groups.append(group.animate(run_time=1.5).move_to([rightShift + i * n, upShift, 0]))
+            #groups.add(AnimationGroup(*[c.animate(run_time=1.5).move_to([transShift+i*n, prevSum, -i]) for c in group]))
+        for i in range(n//2, n - 1, 1):
+            group = cubesCopy[i]
+            groups.append(group.animate(run_time=1.5).move_to([rightShift + upShift, upShift - (i - n//2) * n, 0]))
+        group = cubesCopy[-1]
+        #evenSub1 = VGroup(*[newCubes[1][0].copy(), newCubes[1][1].copy()])
+        return groups
+    
+    def oddSquare(self, n, newCubes, transformShift, prevSum):
+        groups = []
+        rightShift = transformShift + n//2
+        upShift = prevSum + n//2
+        cubesCopy = newCubes.copy()
+        for i in range(n//2):
+            group = cubesCopy[i]
+            groups.append(group.animate(run_time=1.5).move_to([rightShift + i * n, upShift, 0]))
+        for i in range(n//2, n, 1):
+            group = cubesCopy[i]
+            groups.append(group.animate(run_time=1.5).move_to([rightShift + upShift - 1, upShift - (i - n//2) * n, 0]))
+        return groups
     
     def layers(self, n, d):
         layers = VGroup()
@@ -38,7 +70,6 @@ class lec04(ThreeDScene):
     
     def createCubes(self, n, cColor):
         d = VGroup()
-        colorlvl = ['_A', '_B', '_C', '_D', '_E']
         for i in range(n):
             for j in range(n):
                 for k in range(n):
