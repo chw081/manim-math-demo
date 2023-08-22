1fcf272e65d4c4b33ba5b0c50ad4b0ac7356cfd7
chw081 <chw081@ucsd.edu>
2023-08-22 12:05:16 +0800

    lec04

--- lec04.py
+++ lec04.py
@@ -3,8 +3,9 @@ from manim.utils.color import Colors
 
 class lec04(ThreeDScene):
     def construct(self):
-        self.set_camera_orientation(phi=45*DEGREES, theta=-35*DEGREES,zoom=0.6)
-        self.move_camera(frame_center= [4, -0.5, 1])
+        self.set_camera_orientation(phi=35*DEGREES, theta=-25*DEGREES,zoom=0.28)
+        self.move_camera(frame_center= [4, -1.5, 1])
+        formulaText = MathTex(r"1^3 + 2^3 + 3^3 + ... + n^3\\= (1 + 2 + 3 + ... + n)^2", font_size=100).shift(DOWN * 7).shift(RIGHT*12).rotate(65*DEGREES)
         baseCube = Cube(side_length=1, fill_opacity=1, fill_color=RED, stroke_width=0.2, stroke_color=BLACK)
         transformShift = 5
         prevSum = 0
@@ -13,37 +14,22 @@ class lec04(ThreeDScene):
         newCubes = self.createCubes(2, BLUE).shift(UP * 3)
         prevSum += 1
         self.play(FadeIn(newCubes))
-        #groups = VGroup(*[newCubes[i].copy() for i in range(0, 2**3, 2)])
-        #groups = newCubes[0].copy()
-        #groups2slt1 = VGroup(*[newCubes[1][0:2].copy()])
-        #groups2slt2 = VGroup(*[newCubes[1][2:4].copy()])
-        #self.play(
-            #AnimationGroup(*[
-            #group.animate(run_time=1.5).shift(RIGHT * 5, DOWN * 2) for group in groups]),
-            #groups.animate(run_time=1.5).move_to([5.5, 1.5, 0]),
-            #*self.evenSquare(2, newCubes, transformShift, 1),
-            #AnimationGroup(*[
-            #group.animate(run_time=1.5).shift([4, -2, -1]) for group in groups2slt1]),
-            #AnimationGroup(*[
-            #group.animate(run_time=1.5).shift([4.5, -3.5, -1]) for group in groups2slt2.rotate(
-            #90 * DEGREES, axis=[0, 0, 1])]))
-            #groups2slt2.animate(run_time=1.5).move_to([5.5, 0, 0]).rotate(90 * DEGREES, axis=[0, 0, 1]))
         self.play(*self.evenSquare(2, newCubes, transformShift, prevSum))
         newCubes = self.createCubes(3, ORANGE).shift(UP * 7)
         prevSum += 2
         self.play(FadeIn(newCubes))
         self.play(*self.oddSquare(3, newCubes, transformShift, prevSum))
-        newCubes = self.createCubes(4, GREEN).shift(UP * 12)
+        newCubes = self.createCubes(4, GREEN).shift(UP * 18, RIGHT * 6)
         prevSum += 3
         self.play(FadeIn(newCubes))
         self.play(*self.evenSquare(4, newCubes, transformShift, prevSum))
-        newCubes = self.createCubes(5, PURPLE).shift(UP * 18)
+        newCubes = self.createCubes(5, PURPLE).shift(UP * 18, RIGHT * 12)
         prevSum += 4
         self.play(FadeIn(newCubes))
         self.play(*self.oddSquare(5, newCubes, transformShift, prevSum))
-        newCubes = self.createCubes(6, YELLOW).shift(UP * 30)
+        newCubes = self.createCubes(6, YELLOW).shift(UP * 30, LEFT * 10)
         prevSum += 5
-        self.play(FadeIn(newCubes))
+        self.play(FadeIn(formulaText), FadeIn(newCubes))
     
     def evenSquare(self, n, newCubes, transformShift, prevSum):
         groups = []
@@ -87,12 +73,7 @@ class lec04(ThreeDScene):
         for i in range(n):
             for j in range(n):
                 for k in range(n):
-                    #layer_color = Colors(cColor) + colorlvl[k]
                     d.add(Cube(side_length=1, fill_opacity=1, fill_color=cColor, stroke_width=0.2, stroke_color=BLACK).shift([i, j, k]))
-                #d.add(Cube(side_length=1, fill_opacity=0.5, fill_color=cColor).shift(i*UP, col))
-                    #d.add(Cube(side_length=1, fill_opacity=0.5, fill_color=cColor).shift([n - 1, i, j]))
-            #d.append(Square(side_length=0.5).shift(i*RIGHT, row))
-            #d.append(Square(side_length=0.5).shift(i*UP, col))
         d = self.layers(n, d)
         cubes = d.set_color_by_gradient(cColor, WHITE)
         return cubes.set_stroke(color=BLACK)
